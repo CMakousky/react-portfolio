47a18cff8f8fead72d0a047b30f3be421ef12bce
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-02 09:47:21 -0600

    The link for the current page now highlights with a yellow shadow in the Navbar.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,36 @@
 // Bringing in the required import from 'react-router-dom'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import Navbar from './UI/Navbar';
 
 export default function Nav() {
+  // Set up useState to keep track of the current page
+  const [currentPage, setCurrentPage] = useState('About');
+  
+  // Function to update the currentPage useState
+  const handlePageChange = (page) => setCurrentPage(page);
+
   // The Navbar UI component will render each of the Link elements in the links prop
   return (
     <Navbar
       links={[
         <li>
-          <Link key={1} className="nav-link text-light" to="/">
+          <Link key={1} className={currentPage === "About" ? "nav-link active text-light" : "nav-link text-light"} to="/" onClick={() => handlePageChange('About')}>
               About
           </Link>
         </li>,
         <li>
-          <Link key={2} className="nav-link text-light" to="/portfolio">
+          <Link key={2} className={currentPage === "Portfolio" ? "nav-link active text-light" : "nav-link text-light"} to="/portfolio" onClick={() => handlePageChange('Portfolio')}>
               Portfolio
           </Link>
         </li>,
         <li>
-          <Link key={3} className="nav-link text-light" to="/contact">
+          <Link key={3} className={currentPage === "Contact" ? "nav-link active text-light" : "nav-link text-light"} to="/contact" onClick={() => handlePageChange('Contact')}>
               Contact
           </Link>
         </li>,
         <li>
-          <Link key={4} className="nav-link text-light" to="/resume">
+          <Link key={4} className={currentPage === "Resume" ? "nav-link active text-light" : "nav-link text-light"} to="/resume" onClick={() => handlePageChange('Resume')}>
               Resume
           </Link>
         </li>,
