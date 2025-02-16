0f8ddccb226196afcc1b210e589c84372a8a06d0
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-16 14:37:52 -0600

    Split the paragraphs on the AboutMe.jsx page into separate <p> elements.

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -11,12 +11,15 @@ function AboutMe() {
                         {/* Short Bio about myself. */}
                         <h3>Christopher Makousky (Software Engineer) - Minneapolis</h3>
                         <p>
-                            Christopher has 4 years of engineering experience that includes positions at Chickasaw Nation Industries (CNI) Aviation and Arctic Slope Regional Corporation (ASRC) Federal as a contractor for the Federal Aviation Administration (FAA). He worked on-site at the Mike Monroney Aeronautical Center (MMAC) in Oklahoma City, Oklahoma where he supported the operation and maintenance of the National Airspace System (NAS), with emphasis on second-level engineering for power systems.<br /><br />
-
-                            He is currently a student at the University of Minnesota, College of Continuing and Professional Studies (CCAPS), Software Development Boot Camp, which is an immersive Full Stack Web Development certificate program. In his small group projects, Christopher is often seen as the git Configuration Management (CM) focal point. In this role, Christopher is responsible for ensuring that all of his project team members use the current baseline and properly check-in their code without overwriting others code. Christopher currently has 20 repositories in his gitHub portfolio.<br /><br />
-
-                            Christopher's substantial experience at the Mike Monroney Aeronautical Center included 4-years integration & test on multiple types of equipment in FAA Power Systems. These equipment included Batteries, Engine Generators, Direct Current Backup System in the National Airspace System (NAS).  Christopher traveled extensively to various sites in the National Airspace System to perform maintenance and upgrades.<br /><br />
-
+                            Christopher has 4 years of engineering experience that includes positions at Chickasaw Nation Industries (CNI) Aviation and Arctic Slope Regional Corporation (ASRC) Federal as a contractor for the Federal Aviation Administration (FAA). He worked on-site at the Mike Monroney Aeronautical Center (MMAC) in Oklahoma City, Oklahoma where he supported the operation and maintenance of the National Airspace System (NAS), with emphasis on second-level engineering for power systems.
+                        </p>
+                        <p>
+                            He is currently a student at the University of Minnesota, College of Continuing and Professional Studies (CCAPS), Software Development Boot Camp, which is an immersive Full Stack Web Development certificate program. In his small group projects, Christopher is often seen as the git Configuration Management (CM) focal point. In this role, Christopher is responsible for ensuring that all of his project team members use the current baseline and properly check-in their code without overwriting others code. Christopher currently has 20 repositories in his gitHub portfolio.
+                        </p>
+                        <p>
+                            Christopher's substantial experience at the Mike Monroney Aeronautical Center included 4-years integration & test on multiple types of equipment in FAA Power Systems. These equipment included Batteries, Engine Generators, Direct Current Backup System in the National Airspace System (NAS).  Christopher traveled extensively to various sites in the National Airspace System to perform maintenance and upgrades.
+                        </p>
+                        <p>
                             Christopher holds a Bachelor of Science in Mechanical Engineering degree from the South Dakota State University and earned an Engineering Intern (EI) certification, from the South Dakota Board of Technical Professions.
                         </p>
                     </div>
