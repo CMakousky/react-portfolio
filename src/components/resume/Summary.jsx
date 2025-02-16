f144764c7b7681c68a5c8fb78dddf0f9671198ca
Christopher Makousky <christopher.makousky@gmail.com>
2025-02-15 23:28:24 -0600

    Changed 'Engineer' to 'Mechanical engineer' at the start of the Resume summary.

--- src/components/resume/Summary.jsx
+++ src/components/resume/Summary.jsx
@@ -3,7 +3,7 @@ const Summary = () => {
         <>
         <h2>Summary</h2>
         <p>
-            Engineer transitioning into a software development role after completing University of Minnesota Coding Boot Camp. Hands-on project experience building both the front-end and back-end of web apps using React.js, Express.js, and other frameworks.  Previous engineering position integrated airport navigation aids power equipment for with the Power Services Group (PSG) at the FAA's Power Training and Support Facility (PTSF) in Oklahoma City. Focused on equipment from Kohler, provider of engine generator systems (EGS), and Eaton Power Systems, provider of uninterruptible power supply systems (UPS).
+            Mechanical engineer transitioning into a software development role after completing University of Minnesota Coding Boot Camp. Hands-on project experience building both the front-end and back-end of web apps using React.js, Express.js, and other frameworks.  Previous engineering position integrated airport navigation aids power equipment for with the Power Services Group (PSG) at the FAA's Power Training and Support Facility (PTSF) in Oklahoma City. Focused on equipment from Kohler, provider of engine generator systems (EGS), and Eaton Power Systems, provider of uninterruptible power supply systems (UPS).
         </p>
         </>
     );
