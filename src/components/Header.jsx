6854f3dd165d56f4d02633475e25c8c8eed94039
Christopher Makousky <christopher.makousky@gmail.com>
2025-03-04 01:16:44 -0600

    Corrected Navbar keys in 'Header.jsx' by moving them from the <Link> props to the <li> tag.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,23 +14,23 @@ export default function Header() {
   return (
     <Navbar
       links={[
-        <li>
-          <Link key={1} className={currentPage === "About" ? "nav-link active text-light" : "nav-link text-light"} id='about-link' to="/" onClick={() => handlePageChange('About')}>
+        <li key={1}>
+          <Link className={currentPage === "About" ? "nav-link active text-light" : "nav-link text-light"} id='about-link' to="/" onClick={() => handlePageChange('About')}>
               About
           </Link>
         </li>,
-        <li>
-          <Link key={2} className={currentPage === "Portfolio" ? "nav-link active text-light" : "nav-link text-light"} id='portfolio-link' to="/portfolio" onClick={() => handlePageChange('Portfolio')}>
+        <li key={2}>
+          <Link className={currentPage === "Portfolio" ? "nav-link active text-light" : "nav-link text-light"} id='portfolio-link' to="/portfolio" onClick={() => handlePageChange('Portfolio')}>
               Portfolio
           </Link>
         </li>,
-        <li>
-          <Link key={3} className={currentPage === "Contact" ? "nav-link active text-light" : "nav-link text-light"} id='contact-link' to="/contact" onClick={() => handlePageChange('Contact')}>
+        <li key={3}>
+          <Link className={currentPage === "Contact" ? "nav-link active text-light" : "nav-link text-light"} id='contact-link' to="/contact" onClick={() => handlePageChange('Contact')}>
               Contact
           </Link>
         </li>,
-        <li>
-          <Link key={4} className={currentPage === "Resume" ? "nav-link active text-light" : "nav-link text-light"} id='resume-link' to="/resume" onClick={() => handlePageChange('Resume')}>
+        <li key={4}>
+          <Link className={currentPage === "Resume" ? "nav-link active text-light" : "nav-link text-light"} id='resume-link' to="/resume" onClick={() => handlePageChange('Resume')}>
               Resume
           </Link>
         </li>,
