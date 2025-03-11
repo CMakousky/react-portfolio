ae9217c8518d26ff061a49df7ecfd002ba06739c
Christopher Makousky <christopher.makousky@gmail.com>
2025-03-10 19:56:45 -0500

    Commented out line 7 of Footer.jsx.

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
             <footer>
                 <a className="nav-link text-light" href="https://github.com/CMakousky">GitHub Profile</a>
                 <a className="nav-link text-light" href="https://www.linkedin.com/in/christophermakousky">LinkedIn Profile</a>
-                <a className="nav-link text-light" href="https://stackoverflow.com/questions">Some Other Profile</a>
+                {/* <a className="nav-link text-light" href="https://stackoverflow.com/questions">Some Other Profile</a> */}
             </footer>
         </>
     );
